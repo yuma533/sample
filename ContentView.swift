a2b7a934782f34809fee9243b3e201d019e207c6
yuma533 <93819369+yuma533@users.noreply.github.com>
2022-06-01 13:11:56 +0900

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calendar2
+//
+//  Created by 小川悠磨 on 6/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!!!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calendar2App.swift
@@ -0,0 +1,17 @@
+//
+//  calendar2App.swift
+//  calendar2
+//
+//  Created by 小川悠磨 on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct calendar2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
